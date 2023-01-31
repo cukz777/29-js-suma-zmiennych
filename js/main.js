bc182149bc9a7e995a54a44e7f8c1ca6fba81b12
marcin <brand.marcin@gmail.com>
2023-01-31 01:23:35 +0000

    zadanie43657

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 5
+let secondNumber = 54
+
+function addition(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+console.log(addition(5, 54));
